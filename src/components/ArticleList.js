a25fc27edbe4040662c94d881fdcc363f571a910
oumamoringa <richard.ouma@student.moringaschool>
2022-09-21 14:51:57 +0300

    Tests passed

---
+++ src/components/ArticleList.js
@@ -0,0 +1,18 @@
+import Article from "./Article";
+
+function ArticleList({id}){
+    return (
+        <main>
+            <Article
+            key={id}
+            />
+                        <Article
+            key={id}
+            />
+                        <Article
+            key={id}
+            />
+        </main>
+    )
+}
+export default ArticleList;
